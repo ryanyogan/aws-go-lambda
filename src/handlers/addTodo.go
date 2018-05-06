9812371873eedadb92e89318fb47ccb10cf0c444
Ryan Yogan <ryanyogan@gmail.com>
2018-05-06 12:53:15 -0700

    Basic little addTado handler, basic AWS DDB provisions

---
+++ src/handlers/addTodo.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/satori/go.uuid"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+)
+
+// Todo defines the structure of a Todo Item
+type Todo struct {
+	ID          string `json:"id"`
+	Description string `json:"description"`
+	Done        bool   `json:"done"`
+	CreatedAt   string `json:"created_at"`
+}
+
+var ddb *dynamodb.DynamoDB
+
+func init() {
+	region := os.Getenv("AWS_REGION")
+	if session, err := session.NewSession(&aws.Config{
+		Region: &region,
+	}); err != nil {
+		fmt.Println(fmt.Sprintf("Failed to connect to AWS: %s", err.Error()))
+	} else {
+		ddb = dynamodb.New(session)
+	}
+}
+
+// AddTodo is a function that will add a new Todo struct to DynamoDB
+func AddTodo(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Println("AddTodo")
+
+	var (
+		id        = uuid.Must(uuid.NewV4()).String()
+		tableName = aws.String(os.Getenv("TODOS_TABLE_NAME"))
+	)
+
+	todo := &Todo{
+		ID:        id,
+		Done:      false,
+		CreatedAt: time.Now().String(),
+	}
+
+	json.Unmarshal([]byte(request.Body), todo)
+
+	item, _ := dynamodbattribute.MarshalMap(todo)
+	input := &dynamodb.PutItemInput{
+		Item:      item,
+		TableName: tableName,
+	}
+
+	if _, err := ddb.PutItem(input); err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       err.Error(),
+			StatusCode: 500,
+		}, nil
+	} else {
+		body, _ := json.Marshal(todo)
+		return events.APIGatewayProxyResponse{
+			Body:       string(body),
+			StatusCode: 200,
+		}, nil
+	}
+}
+
+func main() {
+	lambda.Start(AddTodo)
+}
